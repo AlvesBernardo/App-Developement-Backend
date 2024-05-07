5967e01a1355458c80ef49ac817cc74891612401
Hakan Ege Özerol <hakan.ege.ozerol@student.nhlstenden.com>
2024-05-07 12:39:38 +0200

    First draft of User-Dashboard

---
+++ app/src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.ComboBox
+import javafx.scene.layout.VBox
+import javafx.stage.Stage
+import java.sql.DriverManager
+
+class CourseDropdownApp : Application() {
+
+    private val comboBox by lazy { ComboBox<String>() }
+
+    override fun start(primaryStage: Stage) {
+        val root = VBox()
+        root.children.add(comboBox)
+
+        primaryStage.scene = Scene(root, 300.0, 200.0)
+        primaryStage.title = "Course Dropdown"
+        primaryStage.show()
+
+        loadCourses()
+    }
+
+    private fun loadCourses() {
+        val connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/app_development", "root", "")
+        connection.use { conn ->
+            val statement = conn.createStatement()
+            statement.use { stmt ->
+                val resultSet = stmt.executeQuery("SELECT dtDescription FROM courses")
+                resultSet.use { rs ->
+                    while (rs.next()) {
+                        comboBox.items.add(rs.getString("dtDescription"))
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    Application.launch(CourseDropdownApp::class.java, *args)
+}

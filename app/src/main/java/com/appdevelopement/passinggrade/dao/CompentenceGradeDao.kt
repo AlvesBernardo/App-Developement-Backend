5d50a6ca6957b7a54d41ec964ee13235f72a04f8
Mehdi Sadeghi <mehdi.sadeghi@student.nhlstenden.com>
2024-06-03 21:06:53 +0200

    Now you can change, add or edit the grades with the button

--- app/src/main/java/com/appdevelopement/passinggrade/dao/CompentenceGradeDao.kt
+++ app/src/main/java/com/appdevelopement/passinggrade/dao/CompentenceGradeDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.appdevelopement.passinggrade.models.CompetenceGrade
+
 @Dao
 interface CompentenceGradeDao {
 
@@ -13,4 +14,7 @@ interface CompentenceGradeDao {
 
     @Query("SELECT * FROM CompetenceGrade WHERE idStudent = :studentId")
     suspend fun getStudentCompetenceGrades(studentId: Int): List<CompetenceGrade>
+
+    @Query("SELECT * FROM CompetenceGrade WHERE idStudent = :studentId AND (SELECT idComptence FROM Compentence WHERE idExam = :idExam) ORDER BY idCompetenceGradeId DESC LIMIT (SELECT COUNT(idExam) FROM Compentence WHERE idExam = :idExam)")
+    suspend fun getGradesForStudentAndExam(studentId: Int, idExam: Int): List<CompetenceGrade>
 }
